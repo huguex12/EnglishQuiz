62363511ee714171191692f7f44d6f5adcedb2eb
hugo <htorres12@hotmail.com>
2018-07-05 10:34:20 -0600

    Initial commit

---
+++ app/src/main/java/com/example/arman/tm7100/Interface/ItemClickListener.java
@@ -0,0 +1,8 @@
+package com.example.arman.tm7100.Interface;
+
+import android.view.View;
+
+public interface ItemClickListener {
+    void onClick(View view, int position, boolean isLongClick);
+
+}
---
+++ app/src/main/java/com/example/arman/tm7100/ViewHolder/CategoryViewHolder.java
@@ -0,0 +1,38 @@
+package com.example.arman.tm7100.ViewHolder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.arman.tm7100.Interface.ItemClickListener;
+import com.example.arman.tm7100.R;
+
+import org.w3c.dom.Text;
+
+public class CategoryViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+    public TextView category_name;
+    public ImageView category_image;
+
+    private ItemClickListener itemClickListener;
+
+
+    public CategoryViewHolder(View itemView) {
+        super(itemView);
+        category_image = (ImageView)itemView.findViewById(R.id.category_image);
+        category_name = (TextView)itemView.findViewById(R.id.category_name);
+
+        itemView.setOnClickListener(this);
+    }
+
+    public void setItemClickListener(ItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
+
+    @Override
+    public void onClick(View v) {
+        itemClickListener.onClick(v,getAdapterPosition(),false);
+
+    }
+}
